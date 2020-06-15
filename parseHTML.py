c864d8ca8ea8c91b4ccd1ddda7360e20ec6f503a
Jonathan Lo <64229972+jlo87@users.noreply.github.com>
2020-06-15 14:32:49 -0400

    parseHTML.py upload

---
+++ parseHTML.py
@@ -0,0 +1,18 @@
+# Python code that can parse HTML with the Beautiful Soup Module.
+
+import bs4, requests
+
+def getBookOutletPrice(productUrl): # 
+	res = requests.get(productUrl) # Request to download the page
+	res.raise_for_status() # Raise an Exception if there is an issue
+
+	soup = bs4.BeautifulSoup(res.text, 'html.parser') # Passed html.parser to dismiss the warning.
+	elems = soup.select('#color-panel > div > div:nth-child(2) > div.col-md-8 > table > tbody > tr:nth-child(5) > td:nth-child(2) > strong > span')
+	return elems[0].text
+
+
+
+
+
+price = getBookOutletPrice('https://bookoutlet.com/Store/Details/9781593278786B')
+print('The price is ' + price)
